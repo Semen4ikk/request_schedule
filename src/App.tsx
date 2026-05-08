f66353b9840d75f792188049ba54babced4e78b2
Semen Prokopets <semen2004vuz@yandex.ru>
2026-05-09 00:10:45 +0400

    сделал график

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import ApiLatencyChart from "./conponents/ApiLatencyChart.tsx";
+
+function App() {
+
+
+  return (
+
+      <section id="center">
+          <ApiLatencyChart />
+      </section>
+  )
+}
+
+export default App
